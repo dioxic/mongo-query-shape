2682ab0aed0e9194d15c66540d0aeb78b9cdad19
Mark Baker-Munton <dioxic@gmail.com>
2026-02-05 13:46:49 +0000

    exclude system collections

--- src/main/kotlin/org/mongo/mqs/repository/QueryStatRepository.kt
+++ src/main/kotlin/org/mongo/mqs/repository/QueryStatRepository.kt
@@ -1,5 +1,7 @@
 package org.mongo.mqs.repository
 
+import com.mongodb.client.model.Aggregates.match
+import com.mongodb.client.model.Filters.nin
 import org.bson.Document
 import org.bson.conversions.Bson
 import org.mongo.mqs.db.MongoClientProvider
@@ -13,7 +15,8 @@ class QueryStatRepository {
     }
     fun getQueryStats(): List<QueryStat> {
         val pipeline = listOf(
-            queryStats()
+            queryStats(),
+            match(nin("key.queryShape.cmdNs.db", listOf("config", "local", "admin")))
         )
         return database.aggregate(pipeline, QueryStat::class.java).toList()
     }
